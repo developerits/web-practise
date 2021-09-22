0a76f25adb366e501b2cc7d565e7d7bc478cb6c4
Vasiliy Yuryev <uv50@yandex.ru>
2021-09-22 12:24:07 +0300

    step 1 of optimization

--- 48/src/js/script.js
+++ 48/src/js/script.js
@@ -74,3 +74,104 @@ function toggleSlide(item, i) {
     itemList[i].classList.toggle("catalog-item__list_active");
   });
 }
+
+//Modal
+$(document).ready(function () {
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("fast");
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("fast");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("fast");
+    });
+  });
+
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+
+      messages: {
+        name: "Пожалуйста, введите своё имя",
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введён адрес почты",
+        },
+      },
+    });
+  }
+
+  valideForms("#order form");
+  valideForms("#consultation-form");
+  valideForms("#consultation form");
+});
+
+document.querySelectorAll(".input-phone").forEach(function (el) {
+  new Cleave(el, { phone: true, phoneRegionCode: "ru" });
+});
+
+const forms = document.querySelectorAll("form");
+forms.forEach(function (el) {
+  el.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Указываем путь до файла на сервере, который будет обрабатывать наш запрос
+    const url = "../mailer/smart.php";
+    let formData = new FormData(el);
+    console.log(getQueryString(formData));
+    /* Указываем что соединение	у нас будет POST, говорим что путь к файлу в переменной url, и что запрос у нас
+асинхронный, по умолчанию так и есть не стоит его указывать, еще есть 4-й параметр пароль авторизации, но этот
+	параметр тоже необязателен.*/
+    // отправим данные
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.send(formData);
+    let inputField = document.querySelectorAll("form input");
+
+    el.reset();
+
+    document.querySelector(".catalog__tab").click();
+
+    $("#consultation, #order").fadeOut("fast");
+    $(".overlay, #thanks").fadeIn("fast");
+
+    xhr.onload = () => alert(xhr.response);
+  });
+});
+
+function getQueryString(formData) {
+  var pairs = [];
+  for (var [key, value] of formData.entries()) {
+    pairs.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+  }
+  return pairs.join("&");
+}
+
+//Smooth scroll and pageUp
+
+window.addEventListener("scroll", function () {
+  // нижняя граница документа
+  let windowRelativeBottom =
+    document.documentElement.getBoundingClientRect().bottom;
+
+  // если пользователь прокрутил достаточно далеко (< 100px до конца)
+  if (windowRelativeBottom < document.documentElement.clientHeight + 700) {
+    $(".pageup").fadeIn();
+  } else {
+    $(".pageup").fadeOut();
+  }
+});
+
+new WOW().init();
