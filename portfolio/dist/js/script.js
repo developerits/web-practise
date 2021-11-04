cee35b7a949dc55362e6f72e56a4b66085fa7813
Vasiliy Yuryev <uv50@yandex.ru>
2021-11-04 21:37:03 +0300

    add portfolio section

--- portfolio/dist/js/script.js
+++ portfolio/dist/js/script.js
@@ -5,3 +5,10 @@ document.querySelector(".menu__close").addEventListener("click", function () {
 document.querySelector(".hamburger").addEventListener("click", function () {
   document.querySelector(".menu").classList.add("active");
 });
+
+const counters = document.querySelectorAll(".technology__progress-text"),
+  lines = document.querySelectorAll(".technology__progress-orange");
+
+counters.forEach((item, i) => {
+  lines[i].style.width = item.innerHTML;
+});
